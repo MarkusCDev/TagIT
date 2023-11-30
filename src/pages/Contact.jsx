6e5c4b830d386773f28c90c281e8936a2955b58d
Myat Thu Ko (Tony) <nymyatthuko@gmail.com>
2023-11-30 14:48:35 -0500

    Make changes on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,10 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import '../customStyles.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../customStyles.css";
 
-{/* Contact Us Page */}
+{
+  /* Contact Us Page */
+}
 
 const Contact = () => {
   return (
@@ -10,7 +12,9 @@ const Contact = () => {
       <div className="page-container-inner">
         {/* Title */}
         <div className="mb-8">
-          <h1 className="text-center text-2xl text-indigo-700 font-bold">Contact Us</h1>
+          <h1 className="text-center text-2xl text-indigo-700 font-bold">
+            Contact Us
+          </h1>
         </div>
 
         {/* Team Members */}
@@ -19,7 +23,7 @@ const Contact = () => {
           <p className="text-indigo-700">Mengwai Chan</p>
           <p className="text-indigo-700">Markus Chmiel</p>
           <p className="text-indigo-700">Myat Thu Ko</p>
-          <p className="text-indigo-700">Kevin Perchersky</p>
+          <p className="text-indigo-700">Kevin Pechersky</p>
         </div>
 
         <div className="mb-6">
@@ -29,33 +33,60 @@ const Contact = () => {
 
         {/* Contact Info */}
         <div className="mb-6">
-          <h2 className="text-xl text-black font-bold">Visit Us</h2>
-          <p className="text-indigo-700">Compton-Goethals Building</p>
-          <p className="text-indigo-700">Room CG04</p>
-          <br />
-          <p className="text-indigo-700">160 Convent Avenue</p>
-          <p className="text-indigo-700">New York, NY 10031</p>
+          <h2 className="text-xl text-black font-bold">
+            Facility Department Contact
+          </h2>
           <p className="text-indigo-700">Phone: (212) 650-8675</p>
           <p className="text-indigo-700">Email: facilities@ccny.cuny.edu</p>
-        </div>
-        <div className="mb-6">
-          <h2 className="text-xl text-black font-bold">Contact Us</h2>
-          <p className="text-indigo-700">Phone: (212) 650-8675</p>
-          <p className="text-indigo-700">Email: facilities@ccny.cuny.edu</p>
-        </div>
-        <div className="mb-6">
-          <h2 className="text-xl text-black font-bold">Visit our website</h2>
-          <a href="https://www.ccny.cuny.edu/facilities" target="_blank" className="text-indigo-700">Go To Facilities website</a>
+          <p class="text-indigo-700">
+            To learn more about Facility Department,{" "}
+            <a
+              href="#"
+              class="inline-flex items-center font-medium text-indigo-700 hover:underline"
+            >
+              visit the website
+              <svg
+                class="w-4 h-4 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </p>
         </div>
 
-        {/*Call To Action About Us Button */}
-        <div className="flex items-center justify-center">
-            <Link to="/about">
-              <button className="button-hover" type="button">About Us</button>
-            </Link>
+        <div className="flex items-center justify-center justify-between">
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeReao8WevqZm-ey8ZCqqdjI124Gt0Zzj4YQcfhXcoR1MGtGg/viewform"
+            target="_blank"
+          >
+            <button
+              className="bg-indigo-700 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition-colors duration-300"
+              type="button"
+            >
+              Feedback Form
+            </button>
+          </a>
+          <Link to="/about">
+            <button
+              className="bg-indigo-700 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition-colors duration-300"
+              type="button"
+            >
+              About
+            </button>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
-export default Contact
+  );
+};
+export default Contact;
