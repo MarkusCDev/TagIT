08e9057a0ad13dab9c515d67aa8ce41086dca768
JohanDelao <johandelao10@gmail.com>
2024-03-28 10:00:59 -0400

    fix safari issue

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Logo = () => {
     const navigate = useNavigate();
     return (
-        <div className="h-16 cursor-pointer" onClick={() => navigate("/")}>
+        <div className="h-16 cursor-pointer max-w-28" onClick={() => navigate("/")}>
             <img src={LogoImage} className="object-contain h-full w-full" alt="Logo of TagIT w/ CCNY" />
         </div>
     )
