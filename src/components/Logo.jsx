fb3e04ddbef5da6915a210cf318ad396b0e4860b
JohanDelao <johandelao10@gmail.com>
2024-03-28 10:38:44 -0400

    fix max-width

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Logo = () => {
     const navigate = useNavigate();
     return (
-        <div className="h-16 cursor-pointer max-w-28" onClick={() => navigate("/")}>
+        <div className="h-16 cursor-pointer max-w-[120px]" onClick={() => navigate("/")}>
             <img src={LogoImage} className="object-contain h-full w-full" alt="Logo of TagIT w/ CCNY" />
         </div>
     )
